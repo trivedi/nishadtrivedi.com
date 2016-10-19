707963db62c12ca1708dbef0ffb53fb2b23fde33
Nishad Trivedi <nishad.d.trivedi@gmail.com>
2016-10-19 01:58:28 -0500

    Add NBA Popularity Trends project

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/projects/nba-trends')
+def graph():
+    return render_template('nba-trends.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
