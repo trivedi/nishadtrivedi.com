1f6a6821a11d713cbcf1a11b1cc3ecd2ffd3a3ef
Nishad Trivedi <nishad.d.trivedi@gmail.com>
2016-10-20 19:28:00 -0500

    Change layout and directory structure

--- app.py
+++ app.py
@@ -8,9 +8,24 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog/index.html', title='Blog')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects/index.html', title='Projects')
+
+
 @app.route('/projects/nba-trends')
-def graph():
-    return render_template('nba-trends.html')
+def nba_trends():
+    return render_template('projects/nba-trends.html')
 
 
 if __name__ == '__main__':
